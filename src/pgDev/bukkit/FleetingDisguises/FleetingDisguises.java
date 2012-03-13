3626a2d22218b33b759d963e9d50906145f1e82a
Devil boy <kws512@hotmail.com>
2012-03-12 20:45:20 -0700

    Implemented configuration

--- src/pgDev/bukkit/FleetingDisguises/FleetingDisguises.java
+++ src/pgDev/bukkit/FleetingDisguises/FleetingDisguises.java
@@ -1,6 +1,8 @@
 package pgDev.bukkit.FleetingDisguises;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Properties;
 
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
@@ -27,6 +29,9 @@ public class FleetingDisguises extends JavaPlugin {
     
     // Listeners
     FDMainListener mainListener = new FDMainListener(this);
+    
+    // Plugin Configuration
+    FDConfig pluginSettings;
 	
 	public void onEnable() {
 		// Check for the plugin directory (create if it does not exist)
@@ -37,6 +42,25 @@ public class FleetingDisguises extends JavaPlugin {
 				System.out.println("New FleetingDisguises directory created!");
 			}
 		}
+		
+		// Load the Configuration
+    	try {
+        	Properties preSettings = new Properties();
+        	if ((new File(pluginConfigLocation)).exists()) {
+        		preSettings.load(new FileInputStream(new File(pluginConfigLocation)));
+        		pluginSettings = new FDConfig(preSettings, this);
+        		if (!pluginSettings.upToDate) {
+        			pluginSettings.createConfig();
+        			System.out.println("FleetingDisguises Configuration updated!");
+        		}
+        	} else {
+        		pluginSettings = new FDConfig(preSettings, this);
+        		pluginSettings.createConfig();
+        		System.out.println("FleetingDisguises Configuration created!");
+        	}
+        } catch (Exception e) {
+        	System.out.println("Could not load FleetingDisguises configuration! " + e);
+        }
 		
 		// Register our events
 		PluginManager pm = getServer().getPluginManager();
