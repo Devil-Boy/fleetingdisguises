a3957194f4ec517983551012358193d12bb4c3b8
Devil boy <kws512@hotmail.com>
2012-03-29 00:21:31 -0700

    Updated for DisguiseCraft 2.0 (allows Enderman holding blocks)

--- src/pgDev/bukkit/FleetingDisguises/FDMainListener.java
+++ src/pgDev/bukkit/FleetingDisguises/FDMainListener.java
@@ -22,23 +22,26 @@ public class FDMainListener implements Listener {
 	public void onDisguise(PlayerDisguiseEvent event) {
 		if (!event.isCancelled()) {
 			Player player = event.getPlayer();
-			if (plugin.undisguiseTimers.containsKey(player.getName())) {
-				event.setCancelled(true);
-				player.sendMessage(ChatColor.RED + "You are still disguised.");
-			} else if (!plugin.hasPermissions(player, "fleetingdisguises.cooldown.exempt") &&
-				plugin.coolDB.containsKey(player.getName()) &&
-				(new Date()).getTime() <= plugin.coolDB.get(player.getName()) + plugin.pluginSettings.disguiseCool * 1000) {
-				event.setCancelled(true);
-				player.sendMessage(ChatColor.RED + "You must wait " + ((plugin.coolDB.get(player.getName()) + plugin.pluginSettings.disguiseCool * 1000 - (new Date()).getTime()) / 1000) + " seconds before disguising again.");
-			} else {
-				if (!plugin.hasPermissions(player, "fleetingdisguises.timelimit.exempt")) {
-					Timer t = new Timer();
-					if (event.getDisguise().isPlayer()) {
-						t.schedule(new UndisguiseTimer(player, plugin), plugin.pluginSettings.disguiseLengths.get("player") * 1000);
-					} else {
-						t.schedule(new UndisguiseTimer(player, plugin), plugin.pluginSettings.disguiseLengths.get(event.getDisguise().mob.name().toLowerCase()) * 1000);
+			Byte endermanHold = event.getDisguise().getHolding();
+			if (endermanHold == null) {
+				if (plugin.undisguiseTimers.containsKey(player.getName())) {
+					event.setCancelled(true);
+					player.sendMessage(ChatColor.RED + "You are still disguised.");
+				} else if (!plugin.hasPermissions(player, "fleetingdisguises.cooldown.exempt") &&
+					plugin.coolDB.containsKey(player.getName()) &&
+					(new Date()).getTime() <= plugin.coolDB.get(player.getName()) + plugin.pluginSettings.disguiseCool * 1000) {
+					event.setCancelled(true);
+					player.sendMessage(ChatColor.RED + "You must wait " + ((plugin.coolDB.get(player.getName()) + plugin.pluginSettings.disguiseCool * 1000 - (new Date()).getTime()) / 1000) + " seconds before disguising again.");
+				} else {
+					if (!plugin.hasPermissions(player, "fleetingdisguises.timelimit.exempt")) {
+						Timer t = new Timer();
+						if (event.getDisguise().isPlayer()) {
+							t.schedule(new UndisguiseTimer(player, plugin), plugin.pluginSettings.disguiseLengths.get("player") * 1000);
+						} else {
+							t.schedule(new UndisguiseTimer(player, plugin), plugin.pluginSettings.disguiseLengths.get(event.getDisguise().mob.name().toLowerCase()) * 1000);
+						}
+						plugin.undisguiseTimers.put(player.getName(), t);
 					}
-					plugin.undisguiseTimers.put(player.getName(), t);
 				}
 			}
 		}
